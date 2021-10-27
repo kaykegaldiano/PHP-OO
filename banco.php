77708733e065ef3b19ae55af71b8a8382b7216e7
Kayke Galdiano <kaykegaldiano@gmail.com>
2021-10-27 00:57:58 -0300

    definindo o cpf e o nome do titular e em seguida recuperando esses dados

--- banco.php
+++ banco.php
@@ -3,6 +3,12 @@
 require_once 'src/Conta.php';
 
 $primeiraConta = new Conta();
-$primeiraConta->depositar(500);
-$primeiraConta->sacar(300); // isso é ok
+$primeiraConta->deposita(500);
+$primeiraConta->saca(300); // isso é ok
 // $primeiraConta->saldo -= 300; // isso não é ok
+$primeiraConta->defineCpfTitular('454.104.348-38');
+$primeiraConta->defineNomeTitular('Kayke');
+
+echo $primeiraConta->recuperaSaldo();
+echo $primeiraConta->recuperaCpfTitular();
+echo $primeiraConta->recuperaNomeTitular();
