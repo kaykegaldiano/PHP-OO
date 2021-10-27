c6df434df2667414b293a07c616985cfd6fbbaa9
Kayke Galdiano <kaykegaldiano@gmail.com>
2021-10-27 01:57:10 -0300

    criando atributo estático para incrementar cada vez que uma conta for criada; criando um método privado para validar o nome do titular; criando um metodo estatico para recuperar o numero de contas

--- src/Conta.php
+++ src/Conta.php
@@ -5,12 +5,17 @@ class Conta
     private string $cpfTitular;
     private string $nomeTitular;
     private float $saldo;
+    private static int $numeroDeContas = 0;
 
     public function __construct(string $cpfTitular, string $nomeTitular)
     {
         $this->cpfTitular = $cpfTitular;
+        $this->validaNomeTitular($nomeTitular);
         $this->nomeTitular = $nomeTitular;
         $this->saldo = 0;
+
+        // incrementa a cada vez que uma conta é criada
+        self::$numeroDeContas++;
     }
 
     public function saca(float $valorASacar): void
@@ -59,5 +64,19 @@ class Conta
     public function recuperaNomeTitular(): string
     {
         return $this->nomeTitular;
+    }
+
+    private function validaNomeTitular(string $nomeTitular)
+    {
+        if (strlen($nomeTitular) < 5) {
+            echo "Nome precisa ter pelo menos 5 caracteres";
+            exit;
+        }
+    }
+
+    // metodo estatico para recuperar o número de contas criadas
+    public static function recuperaNumeroDeContas(): int
+    {
+        return self::$numeroDeContas;
     }
 }
