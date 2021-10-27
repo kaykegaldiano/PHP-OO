e6f9c11e39ceb1c5e0f0017b79738aea4f060621
Kayke Galdiano <kaykegaldiano@gmail.com>
2021-10-27 14:30:28 -0300

    alteração no método construtor para colocar o endereço no cadastro e criação do método getEndereco

--- src/Titular.php
+++ src/Titular.php
@@ -4,13 +4,15 @@ class Titular
 {
     private CPF $cpf;
     private string $nome;
+    private Endereco $endereco;
 
     // construtor da classe
-    public function __construct(CPF $cpf, string $nome)
+    public function __construct(CPF $cpf, string $nome, Endereco $endereco)
     {
         $this->cpf = $cpf;
         $this->validaNomeTitular($nome);
         $this->nome = $nome;
+        $this->endereco = $endereco;
     }
 
     // getter do cpf
@@ -31,5 +33,10 @@ class Titular
             echo "Nome precisa ter pelo menos 5 caracteres";
             exit;
         }
+    }
+
+    public function getEndereco()
+    {
+        return $this->endereco;
     }
 }
