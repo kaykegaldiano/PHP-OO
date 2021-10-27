9c0e44d5e94d62673d149699aaf83915c6c0179e
Kayke Galdiano <kaykegaldiano@gmail.com>
2021-10-27 15:52:32 -0300

    chamando o construtor da classe base (Pessoa)

--- src/Model/Titular.php
+++ src/Model/Titular.php
@@ -8,9 +8,8 @@ class Titular extends Pessoa
     // construtor da classe
     public function __construct(CPF $cpf, string $nome, Endereco $endereco)
     {
-        $this->cpf = $cpf;
-        $this->validaNomeTitular($nome);
-        $this->nome = $nome;
+        // chamando a construtor da classe base (Pessoa)
+        parent::__construct($nome, $cpf);
         $this->endereco = $endereco;
     }
 
