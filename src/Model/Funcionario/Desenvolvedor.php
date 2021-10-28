8cf8e539c3f58fc1b59bfc66995c512e9ed2224e
Kayke Galdiano <kaykegaldiano@gmail.com>
2021-10-28 15:09:37 -0300

    criando a classe Desenvolvedor

---
+++ src/Model/Funcionario/Desenvolvedor.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Alura\Banco\Model\Funcionario;
+
+class Desenvolvedor extends Funcionario
+{
+    public function sobeDeNivel() {
+        $this->recebeAumento($this->getSalario() * 0.75);
+    }
+
+    public function calculaBonificacao(): float
+    {
+        return 500.0;
+    }
+}
