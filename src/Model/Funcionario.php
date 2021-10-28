08f10049325cf8be56d21c19bf5733bb7c06b237
Kayke Galdiano <kaykegaldiano@gmail.com>
2021-10-28 13:33:26 -0300

    adicionando salario e bonificação ao funcionario

--- src/Model/Funcionario.php
+++ src/Model/Funcionario.php
@@ -6,12 +6,14 @@ namespace Alura\Banco\Model;
 class Funcionario extends Pessoa
 {
     private string $cargo;
+    private float $salario;
 
-    public function __construct(string $nome, CPF $cpf, string $cargo)
+    public function __construct(string $nome, CPF $cpf, string $cargo, float $salario)
     {
         // chamando a construtor da classe base (Pessoa)
         parent::__construct($nome, $cpf);
         $this->cargo = $cargo;
+        $this->salario = $salario;
     }
 
     public function getCargo(): string
@@ -23,5 +25,15 @@ class Funcionario extends Pessoa
     {
         $this->validaNomeTitular($nome);
         $this->nome = $nome;
+    }
+
+    public function getSalario() : float
+    {
+        return $this->salario;
+    }
+
+    public function calculaBonificacao(): float
+    {
+        return $this->salario * 0.1;
     }
 }
