9f64d6f72be25540d6849cb6a541790ff8d7b7b9
Kayke Galdiano <kaykegaldiano@gmail.com>
2021-10-28 15:29:56 -0300

    adicionando um novo funcionario editor de vídeo

--- bonificacoes.php
+++ bonificacoes.php
@@ -3,7 +3,7 @@
 require_once 'autoload.php';
 
 use Alura\Banco\Model\CPF;
-use Alura\Banco\Model\Funcionario\{Desenvolvedor, Gerente, Diretor};
+use Alura\Banco\Model\Funcionario\{Desenvolvedor, Gerente, Diretor, EditorVideo};
 use Alura\Banco\Model\Funcionario\Funcionario;
 use Alura\Banco\Service\ControladorDeBonificacoes;
 
@@ -27,9 +27,16 @@ $umDiretor = new Diretor(
     9000.00
 );
 
+$umEditor = new EditorVideo(
+    'Paulo',
+    new CPF('456.987.231-11'),
+    3500
+);
+
 $controlador = new ControladorDeBonificacoes();
 $controlador->adicionaBonificacaoDe($umFuncionario);
 $controlador->adicionaBonificacaoDe($umaFuncionaria);
 $controlador->adicionaBonificacaoDe($umDiretor);
+$controlador->adicionaBonificacaoDe($umEditor);
 
 echo $controlador->getTotalBonificacoes();
