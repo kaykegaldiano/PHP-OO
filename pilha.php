5ec1e44517ebc824223de640baa7c42c0431dad7
Kayke Galdiano <kaykegaldiano@gmail.com>
2021-11-01 11:55:43 -0300

    multiplas exceções

--- pilha.php
+++ pilha.php
@@ -4,19 +4,22 @@ function funcao1()
 {
     echo 'Entrei na função 1' . PHP_EOL;
     try {
-        $arrayFixo = new SplFixedArray(2);
-        $arrayFixo[3] = 'Valor';
-    } catch(RuntimeException $e){
-        echo "Aconteceu um erro." . PHP_EOL;
+        funcao2();
+    } catch(RuntimeException | DivisionByZeroError $e) {
+        echo 'Erro: ' . $e->getMessage() . PHP_EOL;
+        echo 'Linha ' . $e->getLine() . PHP_EOL;
+        echo $e->getTraceAsString() . PHP_EOL;
     }
-    // $divisao = intdiv(5,0);
-    funcao2();
     echo 'Saindo da função 1' . PHP_EOL;
 }
 
 function funcao2()
 {
     echo 'Entrei na função 2' . PHP_EOL;
+
+    $divisao = intdiv(5,0);
+    $arrayFixo = new SplFixedArray(2);
+    $arrayFixo[3] = 'Valor';
     for ($i = 1; $i <= 5; $i++) {
         echo $i . PHP_EOL;
     }
